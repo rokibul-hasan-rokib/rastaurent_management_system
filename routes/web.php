fa29082ce972ef35dfe83c054940505791e5a733
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-06-05 07:14:12 +0600

    home route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.home.home');
-});
+
+Route::get('/',[HomeController::class, 'index'])->name('home');

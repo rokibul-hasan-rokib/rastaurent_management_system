403e781f0370ac18cd68dd938e5fff8381bcf3ad
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-06-05 07:08:09 +0600

    frontend setup rastaurent management system

---
+++ resources/views/frontend/layouts/css.blade.php
@@ -0,0 +1,11 @@
+
+  <link rel="stylesheet" href="{{asset('assets/vendor/animate.css/animate.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/aos/aos.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}">
+
+    <!-- Template Main CSS File -->
+  <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>@yield('title')</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+@include('frontend.layouts.css')
+</head>
+
+<body>
+
+    @include('frontend.layouts.header')
+
+    @yield('content')
+
+    @include('frontend.layouts.footer')
+
+    @include('frontend.layouts.js')
+</body>
+
+</html>

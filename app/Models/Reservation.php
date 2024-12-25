3cc95af7d53fb6a2a4764a9f3f1d9cc155cae32f
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2024-12-25 23:56:59 +0600

    preparedata

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Reservation extends Model
 {
@@ -18,4 +19,14 @@ class Reservation extends Model
         'message',
         'status',
     ];
+
+    private function prepare_data(Request $request): array
+    {
+        return [
+            "name" => $request->input('name'),
+            "email" => $request->input("email"),
+            "phone" => $request->input("phone"),
+            "date" => $request->input("date"),
+        ];
+    }
 }

459f5815ff68af62518aa6822ce422883757b589
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-03-10 02:04:01 +0600

    database design

---
+++ app/Models/SpecialMenu.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpecialMenu extends Model
+{
+    use HasFactory;
+}

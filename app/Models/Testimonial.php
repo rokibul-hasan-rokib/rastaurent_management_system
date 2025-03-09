465f0e28ddbed9d473fade74b343aa0a1041ee79
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-03-10 02:14:48 +0600

    testimonial code done

--- app/Models/Testimonial.php
+++ app/Models/Testimonial.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Testimonial extends Model
 {
@@ -23,8 +24,35 @@ class Testimonial extends Model
     }
 
 
-    final public function getAchievements()
+    final public function getAllTestimonial()
     {
         return self::query()->get();
+    }
+
+    private function prepareData(Request $request, $existingImage = null): array
+    {
+        return [
+            "name" => $request->input('name'),
+            "designation" => $request->input("designation"),
+            "description" => $request->input("description"),
+            'image' => $request->hasFile('image')
+                ? $request->file('image')->store('images', 'public')
+                : $existingImage,
+        ];
+    }
+
+    final public function storeTestimonial(Request $request)
+    {
+        return self::query()->create($this->prepareData($request));
+    }
+
+    final public function updateTestimonial(Request $request, Testimonial $testimonial)
+    {
+        return $testimonial->update($this->prepareData($request));
+    }
+
+    final public function deleteTestimonial(Testimonial $testimonial)
+    {
+        return $testimonial->delete();
     }
 }

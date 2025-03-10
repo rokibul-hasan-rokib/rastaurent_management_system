bb3285ef964b4bff38ac5eedc6ea4dd0d6ff98bc
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-10 15:24:00 +0600

    chef controller ongoing

--- app/Http/Controllers/ChefController.php
+++ app/Http/Controllers/ChefController.php
@@ -7,6 +7,36 @@ use Illuminate\Http\Request;
 class ChefController extends Controller
 {
     public function index(){
-        return view('frontend.chef.chef');
+        $chefs = (new Chefs())->getAllChefs();
+        return view('backend.chef.chef',compact('chefs'));
+    }
+
+    public function create()
+    {
+        return view('backend.chef.create');
+    }
+
+    public function store()
+    {
+        try {
+            DB::beginTransaction(Request $request);
+            $chefs = (new Chefs())->storeChefs($request);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+            //throw $th;
+        }
+    }
+    public function destroy(Chefs $chefs)
+    {
+        try {
+            DB::beginTransaction();
+            (new Chefs())->deleteChefs($chefs);
+            DB::commit();
+            return redirect()->route('chefs.index')->with('success', 'Chefs deleted successfully.');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', $e->getMessage());
+        }
     }
 }
